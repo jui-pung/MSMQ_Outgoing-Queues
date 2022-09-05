e6ada10b18b1a8e2e28b03bcb777b33c1e8fa5ca
JUI-PENG <34677235+jui-pung@users.noreply.github.com>
2022-09-05 21:54:59 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeReview_MSMQ_Chatbot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            myTimer.Tick += new EventHandler(Callback);
+            //使timer可用  
+            myTimer.Enabled = true;
+            //時間間隔  
+            myTimer.Interval = 10000;//10s
+        }
+        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        Task task = new Task();
+        string messageReceiveQueuePath = @".\Private$\chatbox";
+        MessageQueue mq = new MessageQueue();
+        delegate void mydelegate();
+        private void Callback(object sender, EventArgs e)
+        {
+            //目前時間
+            textBoxTimer.Text = DateTime.Now.ToLongTimeString().ToString();
+            WatchQueue();
+            //peekMessageQueuing();
+            //listenQueue();
+        }
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            string messageContent = textBoxSendContent.Text;
+            textBoxReceiveContent.Text += "[I say]: " + task.sendMessageQueuing(messageContent) + "\r\n";
+            //textBoxReceiveContent.Text += "\r\n";
+            //textBoxReceiveContent.Text += DateTime.Now.ToLongTimeString().ToString() + "\r\n";
+        }
+
+        private void buttonReceiveMessage_Click(object sender, EventArgs e)
+        {
+            if (task.peekMessageQueuing() > 0)
+            {
+                textBoxReceiveContent.Text += task.receiveMessageQueuing() + "\r\n";
+                Console.WriteLine(task.peekMessageQueuing());
+            }
+            else
+            {
+                MessageBox.Show("Queue is empty!");
+            }
+        }
+        void WatchQueue()
+        {
+            if (task.peekMessageQueuing() > 0)
+            {
+                mq.Path = messageReceiveQueuePath;
+                //var queue = new MessageQueue(messageQueuePath);
+                mq.Formatter = new XmlMessageFormatter(new string[] { "System.String" });
+                mq.ReceiveCompleted += Queue_ReceiveCompleted;
+                mq.BeginReceive();
+                Console.WriteLine("Receive End!");
+            }
+            else if (task.peekMessageQueuing() < 0)
+            {
+                Console.WriteLine("Queue is empty");
+                //MessageBox.Show("Queue is empty or Queue upper limit!");
+            }
+        }
+        private void Queue_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            Console.WriteLine(e.Message.Body.ToString() + "_" + e.Message.Id);
+
+            if (this.InvokeRequired)
+            {
+                mydelegate d = new mydelegate(DataWatched);
+                this.Invoke(d);
+                mq.BeginReceive();
+                Thread.Sleep(5000);
+            }
+            void DataWatched()
+            {
+                textBoxReceiveContent.Text += "[You say]: " + e.Message.Body.ToString() + "-" + DateTime.Now.ToLongTimeString().ToString() +"\r\n";
+
+            }
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Messaging;
+
+namespace CodeReview_MSMQ_Chatbot
+{
+    public class Task
+    {
+        string messageSengQueuePath = @"FormatName:DIRECT=TCP:172.20.10.2\private$\chatbox";
+        string messageReceiveQueuePath = @".\Private$\chatbox";
+        public string sendMessageQueuing(string messageContent)
+        {
+            try
+            {
+                var queue = new MessageQueue(messageSengQueuePath);
+                queue.Send(messageContent, "Label");
+                Console.WriteLine("Send end!");
+            }
+            catch (MessageQueueException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return messageContent;
+        }
+        public string receiveMessageQueuing()
+        {
+            var queue = new MessageQueue(messageReceiveQueuePath);
+            queue.Formatter = new XmlMessageFormatter(new string[] { "System.String" });
+
+            System.Messaging.Message message = queue.Receive();
+            Console.WriteLine(message.Body);
+            String ms = message.Body.ToString() + "_" + message.Id;
+            return ms;
+        }
+        public int peekMessageQueuing()
+        {
+            int currentMessageNumber = 0;
+            var queue = new MessageQueue(messageReceiveQueuePath);
+            queue.Formatter = new XmlMessageFormatter(new string[] { "System.String" });
+
+            foreach (System.Messaging.Message message in queue)
+            {
+                //Console.WriteLine(message.Body);
+                currentMessageNumber++;
+            }
+            return currentMessageNumber;
+        }
+    }
+}
